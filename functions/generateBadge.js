3fef0606be04269c1de4c1711219e03a4843e503
Gavin Samuels <gavin.samuels10@gmail.com>
2019-08-24 14:56:18 -0500

    added lambda function

---
+++ functions/generateBadge.js
@@ -0,0 +1,33 @@
+const badge = require("gh-badges")
+
+const formatData = ({
+  title = "Default Title",
+  color = "green",
+  value = "Default Value",
+  template = "flat-square",
+}) => {
+  return {
+    text: [` ${title} `, ` ${value} `],
+    colorscheme: color,
+    template: template,
+  }
+}
+
+exports.handler = function(event, context, callback) {
+  const requestBody = JSON.parse(event.query)
+  console.log(event)
+
+  badge(formatData(requestBody), (svg, err) => {
+    var response
+
+    err === undefined ? (response = svg) : (reponse = err)
+
+    callback(null, {
+      statusCode: err ? 500 : 200,
+      headers: {
+        "Content-Type": "image/svg+xml",
+      },
+      body: response,
+    })
+  })
+}
