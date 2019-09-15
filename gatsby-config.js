9cdf3b56c5bd0d872f7be516f3feb551d9f48b7a
Gavin Samuels <gavin.samuels10@gmail.com>
2019-09-15 14:10:17 -0500

    updated theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: `custom-badge`,
         short_name: `badged`,
         start_url: `/`,
-        background_color: `#00c6ff`,
-        theme_color: `#00c6ff`,
+        background_color: `#057C67`,
+        theme_color: `#057C67`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
